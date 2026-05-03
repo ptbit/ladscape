28bfecfb078c7dbbc85cad5c3abe8c08b8484290
YevhenVakhotskyi <yashopua@gmail.com>
2026-05-03 21:52:24 +0300

    modal projects

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const modals = document.querySelectorAll('.modal');
+
+// відкрити модалку
+function openModal(id) {
+  document.getElementById(id).classList.add('active');
+  document.body.style.overflow = 'hidden';
+}
+
+// закрити всі
+function closeAllModals() {
+  modals.forEach((modal) => modal.classList.remove('active'));
+  document.body.style.overflow = '';
+}
+
+document.querySelectorAll('[data-modal]').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    openModal(btn.dataset.modal);
+  });
+});
+
+modals.forEach((modal) => {
+  modal.addEventListener('click', (e) => {
+    if (
+      e.target.classList.contains('modal__overlay') ||
+      e.target.classList.contains('modal__close')
+    ) {
+      closeAllModals();
+    }
+  });
+});
+
+document.querySelectorAll('.form').forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    closeAllModals();
+    openModal('thanks-modal');
+
+    form.reset();
+  });
+});
+
+openModal('projects-modal');
